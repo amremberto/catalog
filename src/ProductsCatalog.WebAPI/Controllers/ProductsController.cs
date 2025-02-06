ed1e6ed92c1d5830c1c471d183d0b4bf9d2d85c3
Remberto Aguilar <raguilar@gcgestion.com.ar>
2025-02-06 17:52:53 +0000

    Add get products by Id

--- src/ProductsCatalog.WebAPI/Controllers/ProductsController.cs
+++ src/ProductsCatalog.WebAPI/Controllers/ProductsController.cs
@@ -7,19 +7,34 @@ namespace ProductsCatalog.WebAPI.Controllers;
 [ApiController]
 public class ProductsController : ControllerBase
 {
+    private static List<Product> products = new List<Product>
+    {
+        new Product { Id = Guid.NewGuid(), Name = "Product 1", Description = "Description 1", Price = 10.5m },
+        new Product { Id = Guid.NewGuid(), Name = "Product 2", Description = "Description 2", Price = 20.5m },
+        new Product { Id = Guid.NewGuid(), Name = "Product 3", Description = "Description 3", Price = 30.5m }
+    };
+
     //CRUD operations
 
     //GET api/products
     [HttpGet]
     public List<Product> Get()
     {
-        var products = new List<Product>
-        {
-            new Product { Id = Guid.NewGuid(), Name = "Product 1", Description = "Description 1", Price = 10.5m },
-            new Product { Id = Guid.NewGuid(), Name = "Product 2", Description = "Description 2", Price = 20.5m },
-            new Product { Id = Guid.NewGuid(), Name = "Product 3", Description = "Description 3", Price = 30.5m }
-        };
-
         return products;
+    }
+
+    //GET api/products/{id}
+    [HttpGet("{id}")]
+    public Product Get(Guid id)
+    {
+        var product = products.Find(p => p.Id == id);
+
+        if (product is null)
+        {
+            var result = new Product();
+            return result;
+        }
+
+        return product;
     }
 }
